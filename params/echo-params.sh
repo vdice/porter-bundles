20dd54f07e1d7088b0248aee54e6997be8d999dc
Vaughn Dice <vadice@microsoft.com>
2021-05-14 09:11:36 -0600

    update params bundle
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- params/echo-params.sh
+++ params/echo-params.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-params="ainteger astringenum astring aboolean installonly sensitive jsonobject"
-
+params="integer string enum boolean installonly sensitive objectstring object array"
 for param in $params; do
   env_var="${param^^}"
   echo "  ${param}: ${!env_var}"
