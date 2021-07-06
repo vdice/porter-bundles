549a575026c3bd79d66ad588f8b9dbd3136056b0
Vaughn Dice <vadice@microsoft.com>
2021-07-06 15:27:19 -0600

    updates to wip/dockerapp
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- wip/dockerapp/helpers.sh
+++ wip/dockerapp/helpers.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euox pipefail
 
-create_volume() {
-  # Create the docker volume if not already present
+create_docker_volume() {
+  # Create Docker volume if it doesn't already exist
   docker volume ls | grep myvol || \
     docker volume create myvol
+
+  # Add files to volume via a helper container
+  docker run -v myvol:/data --name helper busybox true
+  # Could copy the entire app directory in
+  # docker cp /cnab/app helper:/data
+  docker cp /cnab/app/echo.txt helper:/data
+  docker cp /cnab/app/config.toml helper:/data
+  docker rm helper
+}
+
+print_app_logs() {
+  docker logs app_hello_1
 }
 
 # Call the requested function and pass the arguments as-is
